fb8f0573280809b4c27e946c42864328cb2f3e0f
Kevin Mathias <komathias1@gmail.com>
2020-04-27 04:01:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,7 @@ def self.all_students_in_grade_9
       end
   end
 
-def self.first_x_students_in_grade_10(num)
+def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
